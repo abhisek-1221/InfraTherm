cda9ba5f7bf3309d07a05a455e91612c61cbd71e
abhisek1221 <cherrymx317@gmail.com>
2024-11-25 01:49:50 +0530

    file upload component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,14 +4,17 @@ import Features from "./components/features";
 import FAQ from "./components/faqs";
 import AnimatedGridPattern from "@/components/ui/animated-grid-pattern";
 import { cn } from "@/lib/utils";
+import { FileUploadDemo } from "./components/upload";
+import { Button } from "@/components/ui/button";
+import { ArrowBigDown } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <AnimatedGridPattern
-        numSquares={30}
+        numSquares={40}
         maxOpacity={0.1}
-        duration={3}
+        duration={2}
         repeatDelay={1}
         className={cn(
           "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
@@ -20,6 +23,10 @@ export default function Home() {
       />
     <Hero />
     <Features />
+    <Button variant={"secondary"} className="mb-4 py-2 ring-offset-gray-700">
+        Try it out ! Upload your image <ArrowBigDown size={24} /> 
+    </Button>
+    <FileUploadDemo />
     <FAQ />
   </main>
   );
