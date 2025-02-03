d078e46eb327ae196be7dc56e2bdde1e5807fa8e
abhisek1221 <cherrymx317@gmail.com>
2025-02-03 15:24:58 +0530

    deployed render url

--- client/utils/api.ts
+++ client/utils/api.ts
@@ -1,10 +1,10 @@
-const API_URL = 'http://localhost:8000';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/predict';
 
 export async function predictImage(file: File) {
   const formData = new FormData();
   formData.append('file', file);
 
-  const response = await fetch(`${API_URL}/predict`, {
+  const response = await fetch(`${API_URL}`, {
     method: 'POST',
     body: formData,
   });
